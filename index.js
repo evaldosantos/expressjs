5fed733b4215aeb7ce7ff1ad05aa162651f4f071
Evaldo Santos <jesantosjr@protonmail.com>
2021-10-25 21:50:57 -0300

    adding our first expressjs route

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000!");
+});
+
+app.get("/attendances", (req, res) => {
+  res.send("You are on the attendance route!")
+});
