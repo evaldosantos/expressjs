bdbffce1f99dbb46d60085c32bec6b696aeb0cce
Evaldo Santos <jesantosjr@protonmail.com>
2021-10-27 22:35:12 -0300

    criando nossa base de dados com mysql

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const customExpress = require("./config/custom_express");
+const connection = require("./infrastructure/connection");
 
-const app = customExpress();
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000!");
+connection.connect((error) => {
+  if (error) {
+    console.log(error)
+  } else {
+    const app = customExpress();
+    app.listen(3000, () => {
+      console.log("Server running on port 3000!");
+    });
+  }
 });
+
+
