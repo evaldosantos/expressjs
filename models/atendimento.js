76c3aa896b4f7b2e39c5fca75e4cfad2260e27d3
Evaldo Santos <jesantosjr@protonmail.com>
2021-10-28 22:40:36 -0300

    persist dates into db

--- models/atendimento.js
+++ models/atendimento.js
@@ -1,12 +1,16 @@
+const moment = require('moment');
 const connection = require('../infrastructure/connection');
 
 class Atendimento {
   adiciona(atendimento, ) {
+    const dataCriacao = moment().format("YYYY-MM-DD HH:MM:SS");
+    const date = moment(atendimento.atendimentoData, "DD/MM/YYYY").format("YYYY-MM-DD HH:MM:SS");
+    const atendimentoData = { ...atendimento, dataCriacao };
     const sql =  `
       INSERT INTO atendimento set ?
     `;
 
-    connection(sql, (err, result) => {
+    connection(sql, atendimentoData, (err, result) => {
       if (err) {
         console.log(err)
       } else {
